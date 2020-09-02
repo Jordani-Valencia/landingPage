5263379916ebab916e272e4cfb3e642f5909cb6e
jordani valencia rosas <jordani2010@live.com>
2020-09-01 17:13:07 -0700

    Dropdown menu

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,1 +1,8 @@
-window.sr=ScrollReveal();
+function showNav(){
+    let x=document.getElementById("responsive-nav");
+    if(x.className==="responsive-nav main-nav"){
+        x.className+=" unfold";
+    }else{
+        x.className="responsive-nav main-nav";
+    }
+}
