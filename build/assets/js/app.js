8851f530b75193fcc5ab2c8c07c0604db1af0aef
jordani valencia rosas <jordani2010@live.com>
2020-09-02 17:42:42 -0700

    Animations

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -6,3 +6,45 @@ function showNav(){
         x.className="responsive-nav main-nav";
     }
 }
+
+window.sr=ScrollReveal();
+sr.reveal('#header img.phone',{
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px',
+});
+
+sr.reveal('#features div.car',{
+   delay: 200,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px',
+});
+
+sr.reveal('#features div.shoe',{
+   delay: 400,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px',
+});
+
+sr.reveal('#features div.device',{
+   delay: 600,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px',
+});
+
+sr.reveal('#discount .offer',{
+    delay: 200,
+    duration: 300,
+    origin: 'right',
+    distance: '200px',
+});
+
+sr.reveal('#discount picture',{
+    delay: 200,
+    duration: 300,
+    origin: 'left',
+    distance: '200px',
+});
